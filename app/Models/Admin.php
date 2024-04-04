75c91029ccd3bb82eb918f4543c03ee7a3f59720
banajyoti-gratia <banajyoti.gratia@gmail.com>
2024-04-04 12:20:59 +0530

    new changes

---
+++ app/Models/Admin.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticable
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'user_name', 'email', 'password',
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+}
