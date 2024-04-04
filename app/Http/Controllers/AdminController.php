8d97ba1d91cfaad78930b89783b42d4bb16623bc
arshadgratia <arshad.gratia@gmail.com>
2024-04-04 12:37:54 +0530

    testing process 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,20 +7,5 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    public function login(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
-
-        if (Auth::attempt($credentials)) {
-            return response()->json(['success' => true, 'redirect' => '/admin']);
-        } else {
-            return response()->json(['success' => false, 'message' => 'Invalid credentials']);
-        }
-    }
-
-    public function logout()
-    {
-        Auth::logout();
-        return response()->json(['success' => true, 'redirect' => '/login']);
-    }
+    
 }
