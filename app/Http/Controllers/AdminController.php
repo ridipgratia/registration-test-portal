f3754a8431bde8f3eb341d1cd17e799f03f52a6b
arshadgratia <arshad.gratia@gmail.com>
2024-04-04 12:54:45 +0530

    add new 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,16 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
+
 
 class AdminController extends Controller
 {
-    //
-=======
-use Illuminate\Support\Facades\Auth;
 
-class AdminController extends Controller
-{
-    
->>>>>>> 8d97ba1d91cfaad78930b89783b42d4bb16623bc
 }
