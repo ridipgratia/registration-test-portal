0c134cf236739c45f8fb464f3ea99e052e265e81
mafruzgratia <mafruz.gratia@gmail.com>
2024-04-04 14:02:08 +0530

    admin controller updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Suppport\Facades\Auth;
 
 
 class AdminController extends Controller
 {
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email','password');
+
+        if(Auth::guard('admin')->attempt($credentials)) {
+            //authentication passed
+            return redirect()->intended('/admin/home');
+        }
+            return redirect('/admin/login')->with('error', 'Invalid Credentials');  //authentication failed
+    }
 
 }
